1176df81038e8b32fb807bb54561a1fc9b8b087a
Deep Red <0k5786@gmail.com>
2017-08-10 10:53:35 -0400

    Angular routing configuration and back-end integration. Further testing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  devise_for :users
   root to: "dashboard#index"
-  resources :customers, only: [ :index ]
-  
+  # These supercede other /customers routes, so must
+  # come before resource :customers
+  get "customers/ng",                 to: "customers#ng"
+  get "customers/ng/*angular_route",  to: "customers#ng"
+  resources :customers, only: [ :index, :show ]
+  #
 end
