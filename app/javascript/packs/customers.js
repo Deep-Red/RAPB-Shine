a20f6e24cd037a6b5aa4c3e9b6bd1b88815bcd00
Deep Red <0k5786@gmail.com>
2017-08-17 04:13:18 -0400

    Set data to be populated in angular app as it becomes available, including asynchronous request to simulated external database. UI tweaks, including status bar while waiting for CC info.

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -11,43 +11,6 @@ import { CustomerDetailsComponent } from "CustomerDetailsComponent";
 import { CustomerInfoComponent }    from "CustomerDetailsComponent/CustomerInfoComponent"
 import { AddressComponent         } from "CustomerDetailsComponent/AddressComponent"
 import { CreditCardComponent      } from "CustomerDetailsComponent/CreditCardComponent"
-//var RESULTS = [
-//  {
-//    first_name: "Pat",
-//    last_name: "Smith",
-//    username: "psmith",
-//    email: "pat.smith@somewhere.net",
-//    created_at: "2016-02-05",
-//  },
-//  {
-//    first_name: "Patrick",
-//    last_name: "Jones",
-//    username: "pjpj",
-//    email: "jones.p@business.net",
-//    created_at: "2014-03-05",
-//  },
-//  {
-//    first_name: "Patricia",
-//    last_name:  "Benjamin",
-//    username: "pattyb",
-//    email: "benjie@aol.info",
-//    created_at: "2016-01-02",
-//  },
-//  {
-//    first_name: "Patty",
-//    last_name:  "Patrickson",
-//    username: "ppat",
-//    email: "pppp@freemail.computer",
-//    created_at: "2016-02-05",
-//  },
-//  {
-//    first_name: "Jane",
-//    last_name:  "Patrick",
-//    username: "janesays",
-//    email: "janep@company.net",
-//    created_at: "2013-01-05",
-//  }
-//]
 
 var AppComponent = Component({
   selector: "shine-customers-app",
